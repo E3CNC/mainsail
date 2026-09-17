dfb46f4c5ae3dab6440aa03bad5ceec82b45fe78
Isaac Eliape <isaaceliape@gmail.com>
2026-09-17 13:49:08 +0100

    feat(farm): show empty state when no machines registered
    Farm page rendered blank with no remote machines. Adds a v-empty-state
    with locale strings pointing at Settings Remote Printers.

--- src/pages/Farm.vue
+++ src/pages/Farm.vue
@@ -5,16 +5,24 @@
         <v-col v-for="(printer, key) in printers" :key="key" class="v-col-12 v-col-sm-6 v-col-md-4 pb-0">
             <farm-printer-panel :printer="printer"></farm-printer-panel>
         </v-col>
+        <v-col v-if="printerCount === 0" class="v-col-12">
+            <v-empty-state
+                :icon="mdiServerOff"
+                :title="$t('Panels.FarmPrinterPanel.EmptyTitle')"
+                :text="$t('Panels.FarmPrinterPanel.EmptyDescription')" />
+        </v-col>
     </v-row>
 </template>
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useStore } from 'vuex'
 import { useBase } from '@/composables/useBase'
+import { mdiServerOff } from '@mdi/js'
 import FarmPrinterPanel from '@/components/panels/FarmPrinterPanel.vue'
 
 const store = useStore()
 useBase()
 
 const printers = computed(() => store.getters['farm/getPrinters'])
+const printerCount = computed(() => Object.keys(printers.value ?? {}).length)
 </script>
